2339f7e41ccb2d27326f5531c66ed6ae879a09ef
Abhimanyu chaubey <156044459+duryadav@users.noreply.github.com>
2024-10-02 03:08:05 +0530

    Index page of portfolio has be done

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+<script>
+        <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+
+    <!-- Load library from the CDN -->
+    <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+    <!-- Setup and start animation! -->
+    <script>
+            var typed = new Typed('#element', {
+                strings: ['Web developer.', ' Full - Stack developer.' ,'Java developer.'],
+            typeSpeed: 50,
+    });
+    </script>
+</script>
